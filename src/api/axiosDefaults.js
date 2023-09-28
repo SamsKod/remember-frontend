95cabc642e79711c759e33250a2a5294b27f9e80
samuelwigren <samuel.wigren@gsfacket.se>
2023-09-28 14:50:08 +0200

    Change api connection url in axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-//axios.defaults.baseURL = "https://remember-backend-ff8df02164f2.herokuapp.com/";
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://remember-backend-ff8df02164f2.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
